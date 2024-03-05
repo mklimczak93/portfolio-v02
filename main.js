063d38309e504d67e5b82aec7f725bcaa16295c9
magdalena.b.klimczak@gmai.com <magdalena.b.klimczak@gmail.com>
2024-03-05 02:43:30 +0100

    Animations work

--- main.js
+++ main.js
@@ -12,7 +12,24 @@ requestAnimationFrame(raf)
 
 requestAnimationFrame(raf)
 
-
+// --- BARBA --- //
+// barba.init({
+//     sync: true,
+//     transitions: [{
+//         async leave(data) {
+//             const data = this.async();
+//             pageTransition();
+//             await delay(1500);
+//             done();
+//         },
+//         async enter(data) {
+//             contentAnimation();
+//         },
+//         async once(data) {
+//             contentAnimation()
+//         }
+//     }]
+// })
 // --- CURSOR --- //
 //checking if device is touch
 function is_touch_enabled() {
@@ -181,7 +198,27 @@ function horizontalLoop(textClass, direction) {
                 
     }
 }
-function splitOnHover(divId) {
+function showProjectPreview(projectName) {
+    const divElement = document.getElementById('preview-video');
+    if (divElement) {
+        divElement.innerHTML = 
+        `
+        <video width="100%" height="auto" autoplay loop playsinline muted>
+            <source src="assets/projects_photos/${projectName}/${projectName}_movie.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+        `;
+        divElement.style.display = 'flex'
+    }
+}
+function hideProjectPreview(projectName) {
+    const divElement = document.getElementById('preview-video');
+    if (divElement) {
+        divElement.innerHTML = '';
+        divElement.style.display = 'none';
+    }
+}
+function splitOnHover(divId, projectName) {
     const hoverableDiv = document.getElementById(divId);
     if (hoverableDiv) {
         //number splitting
@@ -211,6 +248,9 @@ function splitOnHover(divId) {
             gsap.to('.splitted-text2', {'clip-path': 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', duration: 0.5})
 
         }
+
+        //previewAnimation
+        showProjectPreview(projectName)
     }
         
     
@@ -220,18 +260,122 @@ function unsplitOnHover(divId, projectName, projectNumber) {
     numberElement.innerHTML = "0" + projectNumber;
     let titleElement = document.getElementById(`project-title-${divId}`);
     titleElement.innerHTML = projectName;
+    //previewAnimation
+    hideProjectPreview(projectName)
 }
+
+
+function objectSlideIn(divClass, viewSectionId) {
+    const element = document.querySelector(divClass);
+    console.log('slide')
+    if (element) {
+        gsap.from(divClass, {
+            'clip-path': 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)', 
+            opacity: 0,
+            duration: 2, 
+            ease: 'power3.out', 
+            stagger: 1,
+            scrollTrigger: {
+                        trigger: viewSectionId || none, 
+                        start: "=+200 bottom",
+                        end: "=+500 bottom",
+                        scrub: false,
+                        markers: false,
+                    }
+            })
+    }
+}
+
+function mainObjectSlideIn(divId) {
+    const element = document.querySelector(divId);
+    console.log('slide')
+    if (element) {
+        gsap.set(divId, {
+            //'clip-path': 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)',
+            opacity: 0,
+            yPercent: 40
+        })
+        gsap.to(divId, {
+            //'clip-path': 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', 
+            opacity: 1,
+            yPercent: 0,
+            duration: 4, 
+            ease: 'power3.out', 
+            })
+    }
+}
+
+function floatUp(divId, amount, duration) {
+    const element = document.querySelector(divId);
+    if (element) {
+        gsap.set(divId, {
+            yPercent: amount
+        })
+        gsap.to(divId, {
+            yPercent: 0,
+            duration: duration, 
+            ease: 'linear', 
+            })
+    }
+}
+
+function floatDown(divId, amount, duration) {
+    const element = document.querySelector(divId);
+    if (element) {
+        gsap.set(divId, {
+            yPercent: -amount
+        })
+        gsap.to(divId, {
+            yPercent: 0,
+            duration: duration, 
+            ease: 'linear', 
+            })
+    }
+}
+function slowShowUp(divId, duration) {
+    const element = document.querySelector(divId);
+    if (element) {
+        gsap.set(divId, {
+            opacity: 0
+        })
+        gsap.to(divId, {
+            opacity: 1,
+            duration: duration, 
+            ease: 'power4.in', 
+            })
+    }
+}
+
+function pageTransition() {
+    const tl = gsap.timeline();
+    tl.to('ul.transition li', {
+        duration: .5,
+        scaleY: 1,
+        transformOrigin: "bottom left",
+        stagger: .2,
+    })
+    tl.to('ul.transition li', {
+        duration: .5,
+        scaleY: 0,
+        transformOrigin: "bottom left",
+        stagger: .2,
+        delay: .1
+    })
+}
+
 
 // --- OPEN MAIN PAGE ---//
 function openMainPage(section) {
-    if (document.getElementById('project-full-info').style.display === "flex") {
+    pageTransition()
+    //if (document.getElementById('project-full-info').style.display === "flex") {
         document.getElementById('home').style.display = "flex";
         document.getElementById('projects').style.display = "flex";
         document.getElementById('skills').style.display = "flex";
         document.getElementById('contact').style.display = "flex";
         document.getElementById('project-full-info').style.display = "none";
-    };
-    document.getElementById(section).scrollIntoView();
+    //};
+    slowShowUp('#home', 2)
+    window.scrollTo(0, 0);
     
 };
 
@@ -240,12 +384,15 @@ function openMainPage(section) {
 function openProjectPage(project) {
     //const NewWindow = window.open('./project.html',"_self");
     //NewWindow.onload = () => {}
+    pageTransition()
+    window.scrollTo(0, 0);
     document.getElementById('home').style.display = "none";
     document.getElementById('projects').style.display = "none";
     document.getElementById('skills').style.display = "none";
     document.getElementById('contact').style.display = "none";
     
     const mainDiv = document.getElementById('project-full-info');
+    mainDiv.innerHTML = '';
     mainDiv.style.display = "flex";
     window.scrollTo(0, 0);
 
@@ -286,7 +433,7 @@ function openProjectPage(project) {
         <p class="paragraph">
             ${project.projectDescription}
         </p>
-        <img class="project-main-image" src="./assets/projects_photos/${project.projectName}/${project.projectName}_overview.png">
+        <img class="project-main-image" src="assets/projects_photos/${project.projectName}/${project.projectName}_overview.png">
         <p class="paragraph bolder">
             Main functionalities of the application:
         </p>
@@ -294,10 +441,11 @@ function openProjectPage(project) {
 
         </div>
         <video width="100%" height="auto" autoplay loop playsinline muted>
-            <source src="/assets/projects_photos/${project.projectName}/${project.projectName}_movie.mp4" type="video/mp4">
+            <source src="assets/projects_photos/${project.projectName}/${project.projectName}_movie.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>
         <a href=${project.projectGithubLink} class="regular-button page-visit" data-cursor="pointer2">Github source code</a>
+        <button class="regular-button" data-cursor="pointer2" onclick="openMainPage('projects')" >Go back</button>
         `;
         mainDiv.appendChild(projectInfoDiv);
 
@@ -349,6 +497,13 @@ function openProjectPage(project) {
         }
         
     }
+
+    //applying animations
+    horizontalLoop(".outline-container", 1)
+    mainObjectSlideIn('#project-full-info')
+        
+    
+    
     
 }
 
@@ -365,7 +520,7 @@ window.addEventListener('DOMContentLoaded', async () => {
             newDiv.id = project.id;
             newDiv.setAttribute('data-cursor', 'pointer2');
             newDiv.onclick = () => {openProjectPage(project)}
-            newDiv.onmouseenter = () => {splitOnHover(project.id)}
+            newDiv.onmouseenter = () => {splitOnHover(project.id, project.projectName)}
             newDiv.onmouseleave = () => {unsplitOnHover(project.id, project.projectName, project.id)}
             
             newDiv.innerHTML = 
@@ -412,8 +567,26 @@ window.addEventListener('DOMContentLoaded', () => {
     textDrop('contact-title', 1, '#contact-title');
     
    //mainTitleHorRotation('#outline-fname-01');
-   horizontalLoop(".outline-container", 1)
-   horizontalLoop(".outline-container2", -1)
+   horizontalLoop(".outline-container", 1);
+   horizontalLoop(".outline-container2", -1);
+   objectSlideIn('.skills-div-subgroup', '#skills');
+   //scrolling events
+   let lastScrollTop = document.documentElement.scrollTop;
+   document.addEventListener("scroll", function(){
+    const scrollTopPosition = document.documentElement.scrollTop;
+    if (scrollTopPosition > lastScrollTop) {
+        console.log('scrolling down');
+        // floatUp('#projects', 30, 1);
+        // floatUp('#skills', 30, 1);
+        // floatUp('#contact', 30, 1);
+      } else if (scrollTopPosition < lastScrollTop) {
+        console.log('scrolling up');
+        // floatDown('#projects', 30, 1);
+        // floatDown('#skills', 30, 1);
+        // floatDown('#contact', 30, 1);
+      }
+    
+   })
 });
 
 
